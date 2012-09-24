925001cf92415322e1d1864c7c38fa1bc9585a69
whanderley <whanderley.souza@gmail.com>
2012-09-24 14:57:39 -0300

    modified the code from videosegmentation

--- nsi/granulate/GranulateVideo.py
+++ nsi/granulate/GranulateVideo.py
@@ -3,7 +3,7 @@
 #
 # Copyright (c) 2007 ISrg (NSI, CEFETCAMPOS, BRAZIL) and Contributors.
 #                                                         All Rights Reserved.
-#                              Fábio Duncan de Souza<fduncan@cefetcampos.br>
+#                            Fábio Duncan de Souza<fduncan@cefetcampos.br>
 #
 # WARNING: This program as such is intended to be used by professional
 # programmers who take the whole responsability of assessing all potential
@@ -34,13 +34,12 @@ __docformat__ = 'plaintext'
 import os
 import tempfile
 from StringIO import StringIO
-from shotVideo import ShotVideo, InitExtract
-from GranularUtils import Grain
+import re
 import shutil
-
+from GranularUtils import Grain
+import Image
 
 class Temporary(object):
-
 
     def __init__(self, videoFileName):
         self.videoFile = videoFileName
@@ -50,16 +49,16 @@ class Temporary(object):
         self.filePath = os.path.join(self.tempdir, self.videoFile)
         return self.filePath
 
+ 
     def createFile(self, dataFile):
         open(self.filePath, "w+").write(dataFile.getvalue())
 
+
     def removeDirectory(self):
-        os.remove(self.filePath)
-        os.removedirs(self.tempdir)
+        shutil.rmtree(self.tempdir)
 
 
 class GranulateVideo(object):
-
 
 
     def __init__(self, video_file, **args):
@@ -68,43 +67,44 @@ class GranulateVideo(object):
         self.file = video_file
         self.refresh(**args)
 
-
     def refresh(self, **args):
         self.temporaryFileSystem = Temporary(self.file.getFilename())
         self.temporaryPathVideo = self.temporaryFileSystem.createDirectory()
         self.temporaryFileSystem.createFile(self.file.getData())
+        self.image_path = self.temporaryFileSystem.tempdir + '/segmentation_video/transitions_video'
+        self.temporaryPathGrain = self.temporaryFileSystem.tempdir + '/segmentation_video/parts_videos'
 
         if args.get('sensitivity'):
             self.sensitivityPercent = args['sensitivity']
         else:
-            self.sensitivityPercent = 0.3
+            self.sensitivityPercent = 0.35
 
     def findTransition(self):
-        initExtract = InitExtract()
-        shotVideo = ShotVideo()
-        video_loaded = initExtract.createCapture(self.temporaryPathVideo)
-        list_transition, list_time = shotVideo.shotDetect(video_loaded, self.sensitivityPercent)
-        return list_transition, list_time
+        os.system('videoShot -i ' + self.temporaryPathVideo + ' -o ' + self.temporaryFileSystem.tempdir)
 
-    def cut_video(self, list_time):
-        self.temporaryPathGrain = tempfile.mkdtemp(prefix="temporaryGrainDirectory")
-        for i in range(len(list_time) - 1):
-            init = list_time[i]
-            duration = list_time[i + 1] - init - 0.2
-            os.system("ffmpeg -i "+str(self.temporaryPathVideo) + " -acodec libvorbis -vcodec libtheora -ss "+ str(init) + " -t "+ str(duration)+ " " + self.temporaryPathGrain + "/video" +str(i)+".ogg")
+    def get_transition_time(self, filename):
+        return re.search('\d+.\d*', filename).group()
+
+    def get_image_list(self):
+        image_list = []
+        time_list = []
+        print self.image_path
+        for image in os.listdir(self.image_path):
+            image_list.append(Image.open(self.image_path + '/' + image))
+            time_list.append(self.get_transition_time(image))
+        return image_list, time_list
 
     def granulate(self):
         """
         """
         returnDict = {}
-        imageList, timeList = self.findTransition()
-        self.cut_video(timeList)
-        return_list_image = self.create_image_grains_list(imageList, timeList)
+        self.findTransition()
+        image_list, time_list = self.get_image_list()
+        return_list_image = self.create_image_grains_list(image_list, time_list)
         return_list_video = self.create_video_grains_list()
         returnDict['image_list']=return_list_image
         returnDict['file_list']=return_list_video
         self.temporaryFileSystem.removeDirectory()
-        shutil.rmtree(self.temporaryPathGrain)
         return returnDict
 
     def create_image_grains_list(self, imageList, timeList):
